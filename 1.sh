b7dd810c4bebb53040dd6f3d2ea6e646205b49f8
Gururaj1119 <114040066+Gururaj1119@users.noreply.github.com>
2022-10-15 21:04:16 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Testing auto trigger of Jenkins job using Different Methods"
-echo "Trigger Periodically"
+echo "Build after other projects- here after webwogrok project"
 echo "Success"
